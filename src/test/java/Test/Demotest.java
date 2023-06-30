6abbf4e0193075fbc48c170775df76e7056746be
AKSHAY <AKSHAY@LAPTOP-B86SJ351.bbrouter>
2023-06-30 01:45:09 -0800

    first commit

---
+++ src/test/java/Test/Demotest.java
@@ -0,0 +1,27 @@
+package Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import page.Demopage;
+
+public class Demotest {
+	
+		WebDriver driver;
+		@BeforeTest
+	public void setup()
+	{
+			driver=new ChromeDriver();
+			driver.get("http://www.facebook.com");
+
+	}
+		@Test
+		public void fbtest()
+		{
+			Demopage obj=new Demopage(driver);
+			obj.setvalues("abc12@gmail.com", "abcd");
+			obj.login();
+		}
+
+	}
